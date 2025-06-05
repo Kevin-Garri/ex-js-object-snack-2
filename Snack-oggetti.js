62a21b9912bede8ef34a010d9229a898b4003115
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-06-05 15:37:56 +0200

    add: snack2

--- Snack-oggetti.js
+++ Snack-oggetti.js
@@ -1,4 +1,5 @@
 //1.
+/*
 const hamburger = { name: "Cheese Burger", wheight: 250 };
 const secondBurger = hamburger;
 secondBurger.name = "Double Cheese Burger";
@@ -7,7 +8,24 @@ secondBurger.wheight = 500;
 console.log(hamburger.name); // ?
 console.log(secondBurger.name); // ?
 
+*/
 
 //Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 //Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 1
 
+//2.
+const hamburger = {
+  name: "Cheese Burger",
+  weight: 250,
+  ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // ?
+console.log(secondBurger.ingredients[0]); // ?
+
+//P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
+//Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+//Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? // 3
